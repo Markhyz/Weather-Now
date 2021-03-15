101bede174254950c6086a5ad16a623aed8b18d6
framos <framos@fesctecnologia.com.br>
2021-03-14 21:01:42 -0300

    added application

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="weather-now">
+    <header class="weather-now-header">
+      <img class="weather-now-header__logo" :src="logoImage" />
+    </header>
+    <main class="weather-now-body">
+      <div class="weather-now__card">
+        <WeatherCard city="Nuuk" country-code="GL" />
+      </div>
+      <div class="weather-now__card">
+        <WeatherCard city="Urubici" country-code="BR" expanded />
+      </div>
+      <div class="weather-now__card">
+        <WeatherCard city="Nairobi" country-code="KE" />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from "@/assets/logo.svg";
+import WeatherCard from "@/components/WeatherCard";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    WeatherCard,
+  },
+  data() {
+    return {
+      logoImage: Logo,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+
+:root {
+  --app-background-color: #f1f1f1;
+  --card-footer-color: #f1f1f180;
+  --divider-color: #ebebeb;
+  --box-shadow-color: #33333319;
+  --text-light-color: #b4b4b4;
+  --text-color: #737c84;
+  --blue-color: #69a3ff;
+  --orange-color: #ff9632;
+  --red-color: #ed1946;
+}
+
+label {
+  color: var(--text-light-color);
+  margin-bottom: 4px;
+}
+</style>
+
+<style lang="scss" scoped>
+.weather-now {
+  color: var(--text-color);
+  display: flex;
+  flex-direction: column;
+  font-family: Helvetica, Arial;
+  height: 100vh;
+
+  .weather-now-header {
+    align-items: center;
+    flex-shrink: 0;
+    border-bottom: 1px solid var(--divider-color);
+    display: flex;
+    height: 5%;
+    justify-content: center;
+    padding: 8px 0;
+
+    .weather-now-header__logo {
+      height: 70%;
+    }
+  }
+
+  .weather-now-body {
+    align-items: center;
+    background: var(--app-background-color);
+    display: flex;
+    flex-grow: 1;
+    justify-content: space-between;
+    padding: 0 5%;
+    overflow: auto;
+
+    .weather-now__card {
+      width: 30%;
+    }
+
+    @media only screen and (max-device-width: 768px) {
+      flex-direction: column;
+      padding: 2rem;
+
+      .weather-now__card {
+        width: 100%;
+
+        &:not(:last-child) {
+          padding-bottom: 2rem;
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,287 @@
+<template>
+  <div class="weather-card">
+    <div class="weather-card-header">{{ title }}</div>
+    <div class="weather-card-content">
+      <div class="weather-card-body" @click="toggleExpansion">
+        <span class="weather-card__temperature" :class="temperatureClass">{{
+          temperature
+        }}</span>
+      </div>
+      <div class="weather-card-footer">
+        <div v-show="isExpanded" class="weather-card__extra-weather-info">
+          <div>
+            <label>HUMIDITY</label>
+            {{ humidity }}%
+          </div>
+          <div>
+            <label>PRESSURE</label>
+            {{ pressure }}hPa
+          </div>
+        </div>
+        <div class="weather-card__update-info">
+          {{ updateInfo }}
+        </div>
+      </div>
+      <template v-if="!updateSuccess">
+        <div class="weather-card-error">
+          <span class="weather-card-error__message">
+            Something went wrong
+          </span>
+          <button class="weather-card-error__button" @click="updateWeatherInfo">
+            Try Again
+          </button>
+        </div>
+      </template>
+      <template v-if="isLoading">
+        <div class="weather-card-loading-frame">
+          <img class="weather-card__spinner" :src="spinner" />
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import dayjs from "dayjs";
+
+import Spinner from "@/assets/loader.svg";
+
+export default {
+  name: "WeatherCard",
+  props: {
+    city: String,
+    countryCode: String,
+    expanded: Boolean,
+  },
+  data() {
+    return {
+      temperature: 0,
+      humidity: 0,
+      pressure: 0,
+      updateHandle: null,
+      updateTime: null,
+      updateSuccess: false,
+      spinner: Spinner,
+      isLoading: false,
+      isExpanded: this.expanded,
+    };
+  },
+  computed: {
+    title() {
+      return `${this.city}, ${this.countryCode}`;
+    },
+    temperatureClass() {
+      const temperatureClassPrefix = "weather-card__temperature--";
+
+      if (this.temperature <= 5) {
+        return temperatureClassPrefix + "cold";
+      } else if (this.temperature > 25) {
+        return temperatureClassPrefix + "hot";
+      } else {
+        return temperatureClassPrefix + "normal";
+      }
+    },
+    updateInfo() {
+      return `Updated at ${this.updateTime?.format("hh:mm:ss A")}`;
+    },
+  },
+  mounted() {
+    this.updateWeatherInfo();
+    this.updateHandle = global.setInterval(() => {
+      if (this.updateSuccess) {
+        this.updateWeatherInfo();
+      }
+    }, 3000);
+  },
+  beforeDestroy() {
+    if (this.updateHandle) {
+      global.clearInterval(this.updateHandle);
+    }
+  },
+  methods: {
+    toggleExpansion() {
+      this.isExpanded = !this.isExpanded;
+    },
+    async updateWeatherInfo() {
+      this.isLoading = true;
+
+      const owAPIPrefix = "https://api.openweathermap.org/data/2.5/weather";
+      const owAPIWeatherQuery = `q=${this.city},${this.countryCode}`;
+      const owAPIUnits = "units=metric";
+      const owAPIKey = `appid=${process.env.VUE_APP_OPEN_WEATHER_API_KEY}`;
+
+      const cacheKey = `${this.city.toLowerCase()}/${this.countryCode.toLowerCase()}_weatherInfo`;
+      const cachedWeatherInfo = JSON.parse(
+        global.localStorage.getItem(cacheKey)
+      );
+
+      let weatherInfo;
+
+      if (this.isValidCache(cachedWeatherInfo)) {
+        weatherInfo = cachedWeatherInfo;
+      } else {
+        try {
+          const { data } = await axios.get(
+            `${owAPIPrefix}?${owAPIWeatherQuery}&${owAPIUnits}&${owAPIKey}`
+          );
+          const currentTime = dayjs();
+
+          weatherInfo = {
+            temperature: parseInt(data.main.temp),
+            humidtity: data.main.humidity,
+            pressure: data.main.pressure,
+            updatedAt: currentTime.valueOf(),
+            expiresAt: currentTime.add(10, "s").valueOf(),
+          };
+
+          global.localStorage.setItem(cacheKey, JSON.stringify(weatherInfo));
+        } catch {
+          weatherInfo = null;
+        }
+      }
+
+      if (weatherInfo) {
+        this.updateSuccess = true;
+
+        this.temperature = weatherInfo?.temperature;
+        this.humidity = weatherInfo?.humidtity;
+        this.pressure = weatherInfo?.pressure;
+        this.updateTime = dayjs(weatherInfo?.updatedAt);
+      } else {
+        this.updateSuccess = false;
+      }
+
+      this.isLoading = false;
+    },
+    isValidCache(cachedWeatherInfo) {
+      if (!cachedWeatherInfo) return false;
+
+      const currentTime = dayjs();
+
+      return dayjs(cachedWeatherInfo.expiresAt).isAfter(currentTime);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.weather-card {
+  background: #ffffff;
+  border: 1px solid var(--divider-color);
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px var(--box-shadow-color);
+  text-align: center;
+
+  .weather-card-header {
+    border-bottom: 1px solid var(--divider-color);
+    font-size: 1.2rem;
+    padding: 12px;
+  }
+
+  .weather-card-body {
+    cursor: pointer;
+    padding: 24px;
+
+    .weather-card__temperature {
+      font-size: 6rem;
+
+      &::after {
+        content: "°";
+        font-size: 3rem;
+        line-height: 5rem;
+        vertical-align: top;
+      }
+
+      &.weather-card__temperature--normal {
+        color: var(--orange-color);
+      }
+
+      &.weather-card__temperature--cold {
+        color: var(--blue-color);
+      }
+
+      &.weather-card__temperature--hot {
+        color: var(--red-color);
+      }
+    }
+  }
+
+  .weather-card-footer {
+    background: var(--card-footer-color);
+    padding: 12px;
+
+    label {
+      font-size: 0.8rem;
+    }
+
+    .weather-card__extra-weather-info {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 12px;
+
+      div {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 8px;
+      }
+    }
+
+    .weather-card__update-info {
+      color: var(--text-light-color);
+      font-size: 0.7rem;
+    }
+  }
+
+  .weather-card-content {
+    position: relative;
+
+    .weather-card-error {
+      align-items: center;
+      background: white;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      width: 100%;
+
+      .weather-card-error__message {
+        color: var(--red-color);
+        size: 1.2rem;
+        margin-bottom: 1.2rem;
+      }
+
+      .weather-card-error__button {
+        all: unset;
+        background: white;
+        border: 2px solid black;
+        border-radius: 100px;
+        color: var(--text-color);
+        cursor: pointer;
+        font-size: 1.1rem;
+        padding: 0.6rem 1rem;
+      }
+    }
+
+    .weather-card-loading-frame {
+      align-items: center;
+      background: white;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      width: 100%;
+
+      .weather-card__spinner {
+        width: 25%;
+      }
+    }
+  }
+}
+</style>
